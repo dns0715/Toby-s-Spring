c998a61148a7b4f23b52b1547253ad635344e36a
서운성 <@DESKTOP-T67O1VO>
2017-09-20 23:23:47 +0900

    1.3 170920

---
+++ src/springbook/user/dao/DConnectionMaker.java
@@ -0,0 +1,15 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DConnectionMaker implements ConnectionMaker{
+	@Override
+	public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "root", "1323414");
+		
+		return c;
+	}
+}
